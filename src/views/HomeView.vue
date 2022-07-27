eecc860a6fbc6fee2d74fc6235db8fda65199f8f
NaifHagatulah <naifhagatulah@hotmail.com>
2022-07-27 13:18:08 +0200

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <p>this will not be styled</p>
+    <HelloWorld msg="Hello world"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style scoped>
+p{
+  color: blue;
+}
+</style>
